392a706032606cc8a41bdef13f90482a303abe9f
dwikasmsd <smsdwikadarma.13@upi.edu>
2024-12-10 13:27:50 +0700

    Create floruna.sql

---
+++ floruna.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE floruna_quiz;
+
+USE floruna_quiz;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
